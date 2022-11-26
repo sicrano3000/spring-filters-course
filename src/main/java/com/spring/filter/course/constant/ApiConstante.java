1df46afe253440aec6a328230d22ca4ce8d7f14b
Jonathan Philipe <sicrano3000@gmail.com>
2022-11-26 17:26:54 -0300

    configurando sort

--- src/main/java/com/spring/filter/course/constant/ApiConstante.java
+++ src/main/java/com/spring/filter/course/constant/ApiConstante.java
@@ -4,7 +4,9 @@ public class ApiConstante {
 	
 	public static final String LIMIT_KEY = "limit";
 	public static final String PAGE_KEY = "page";
+	public static final String SORT_KEY = "sort";
 	public static final Integer DEFAULT_LIMIT = 10;
 	public static final Integer DEFAULT_PAGE = 0;
+	public static final String DEFAULT_SORT = "";
 
 }
--- src/main/java/com/spring/filter/course/model/FilterModel.java
+++ src/main/java/com/spring/filter/course/model/FilterModel.java
@@ -1,11 +1,20 @@
 package com.spring.filter.course.model;
 
+import static com.spring.filter.course.constant.ApiConstante.DEFAULT_LIMIT;
+import static com.spring.filter.course.constant.ApiConstante.DEFAULT_PAGE;
+import static com.spring.filter.course.constant.ApiConstante.DEFAULT_SORT;
+import static com.spring.filter.course.constant.ApiConstante.LIMIT_KEY;
+import static com.spring.filter.course.constant.ApiConstante.PAGE_KEY;
+import static com.spring.filter.course.constant.ApiConstante.SORT_KEY;
+
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
-
-import static com.spring.filter.course.constant.ApiConstante.*;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.domain.Sort.Order;
 
 import lombok.Getter;
 import lombok.Setter;
@@ -16,16 +25,42 @@ public class FilterModel {
 	
 	private Integer limit;
 	private Integer page;
+	private String sort;
 	
 	public FilterModel(Map<String, String> params) {
 	
 		this.limit = params.containsKey(LIMIT_KEY) ? Integer.valueOf(params.get(LIMIT_KEY)) : DEFAULT_LIMIT;
 		this.page = params.containsKey(PAGE_KEY) ? Integer.valueOf(params.get(PAGE_KEY)) : DEFAULT_PAGE;
+		this.sort = params.containsKey(SORT_KEY) ? params.get(SORT_KEY) : DEFAULT_SORT;
 		
 	}
 	
 	public Pageable toSpringPageable() {
-		return PageRequest.of(page, limit);
+		var orders = getOrders();
+		
+		return PageRequest.of(page, limit, Sort.by(orders));
+	}
+	
+	private List<Order> getOrders() {
+		List<Order> orders = new ArrayList<>();
+		
+		var properties = sort.split(",");
+		
+		for(String property: properties) {
+			if (!property.trim().isEmpty()) {
+				String column = "";
+				
+				if (property.startsWith("-")) {
+					column = property.replace("-", "").trim();
+					orders.add(Order.desc(column));
+				} else {
+					column = property.trim();
+					orders.add(Order.asc(column));
+				}
+			}
+		}
+		
+		return orders;
 	}
 	
 }
